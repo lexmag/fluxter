52d652c5ff24e404f65949a7d099d0a3b9f70a27
Andrea Leopardi <an.leopardi@gmail.com>
2016-03-17 10:20:34 +0100

    Add a @moduledoc to the Fluxter module

--- lib/fluxter.ex
+++ lib/fluxter.ex
@@ -1,4 +1,68 @@
 defmodule Fluxter do
+  @moduledoc """
+  InfluxDB writer for Elixir that uses InfluxDB's line protocol over UDP.
+
+  To get started with Fluxter, you have to create a module that calls `use
+  Fluxter`, like this:
+
+      defmodule MyApp.Fluxter do
+        use Fluxter
+      end
+
+  This way, `MyApp.Fluxter` becomes an InfluxDB connection pool. Each Fluxter
+  pool provides a `start_link/0` function that starts that pool and connects to
+  InfluxDB; this function needs to be invoked before being able to send data to
+  InfluxDB. Typically, you won't call `start_link/0` directly as you'll want to
+  add Fluxter pools to your application's supervision tree; for example:
+
+      def start(_type, _args) do
+        import Supervisor.Spec
+
+        children = [
+          supervisor(MyApp.Fluxter, []),
+          # ...
+        ]
+        Supervisor.start_link(children, strategy: :one_for_one)
+      end
+
+  Once a Fluxter pool is started, its `write/2,3` and `measure/2,3,4` functions
+  can successfully be used to send points to the data store.
+
+  ## Configuration
+
+  Fluxter can be configured either globally or on a per-pool basis.
+
+  The global configuration will affect all Fluxter pools; it can be specified by
+  configuring the `:fluxter` application:
+
+      config :fluxter,
+        host: "metrics.example.com",
+        port: 1122
+
+  The per-pool configuration can be specified by configuring the pool module
+  under the `:fluxter` application:
+
+      config :fluxter, MyApp.Fluxter,
+        host: "metrics.example.com",
+        port: 1122,
+        pool_size: 10
+
+  The following is a list of all the supported options:
+
+    * `:host` - (binary) the host to send metrics to. Defaults to `"127.0.0.1"`.
+    * `:port` - (integer) the port (on `:host`) to send the metrics to. Defaults
+      to `8092`.
+    * `:prefix` - (binary or `nil`) all metrics sent to the data store through
+      the configured Fluxter pool will be prefixed by the value of this
+      option. If `nil`, metrics will not be prefixed. Defaults to `nil`.
+    * `:pool_size` - (integer) the size of the connection pool for the given
+      Fluxter pool. **This option can only be configured on a per-pool basis**;
+      configuring it globally for the `:fluxter` application has no
+      effect. Defaults to `5`.
+
+  """
+
+  @doc false
   defmacro __using__(_opts) do
     quote [unquote: false, location: :keep] do
       @pool_size Application.get_env(__MODULE__, :pool_size, 5)
