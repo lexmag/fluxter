3b48a568cc0345ddc6a2be8984e394e9df870485
Andrea Leopardi <an.leopardi@gmail.com>
2016-03-17 11:15:17 +0100

    Make Fluxter a behaviour that pools implement

--- lib/fluxter.ex
+++ lib/fluxter.ex
@@ -26,7 +26,9 @@ defmodule Fluxter do
       end
 
   Once a Fluxter pool is started, its `write/2,3` and `measure/2,3,4` functions
-  can successfully be used to send points to the data store.
+  can successfully be used to send points to the data store. A Fluxter pool
+  implements the `Fluxter` behaviour, so you can read documentation for the
+  callbacks the behaviour provides to know more about these functions.
 
   ## Configuration
 
@@ -62,9 +64,107 @@ defmodule Fluxter do
 
   """
 
+  @type field_value :: number | boolean | binary
+  @type tags :: [{String.Chars.t, String.Chars.t}]
+  @type fields :: [{String.Chars.t, field_value}]
+
+  @doc """
+  Starts this Fluxter pool.
+
+  A Fluxter pool is a set of processes supervised by a supervisor; this function
+  starts all that processes and that supervisor.
+
+  Usually, you'll want to use a Fluxter pool in the supervision tree of your
+  application:
+
+      def start(_type, _args) do
+        import Supervisor.Spec
+
+        children = [
+          supervisor(MyApp.Fluxter, []),
+          # ...
+        ]
+        Supervisor.start_link(children, strategy: :one_for_one)
+      end
+
+  """
+  @callback start_link() :: Supervisor.on_start
+
+  @doc """
+  Writes a metric to the data store.
+
+  `name` is the name of the metric to write. `tags` is a list of key-value pairs
+  that specifies tags (as name and value) for the metric to write; note that tag
+  values are converted to strings as InfluxDB only support string values for
+  tags. `fields` can either be a list of key-value pairs, in which case it
+  specifies a list of fields (as name and value), or a single value
+  (specifically, a boolean, float, integer, or binary). In the latter case, the
+  default field name of `value` will be used: calling `write("foo", [], 4.3)` is
+  the same as calling `write("foo", [], value: 4.3)`.
+
+  The return value is always `:ok` as writing is a *fire-and-forget* operation.
+
+  ## Examples
+
+  Assuming a `MyApp.Fluxter` Fluxter pool exists:
+
+      iex> MyApp.Fluxter.write("cpu_temp", [host: "eu-west"], 68)
+      :ok
+
+  """
+  @callback write(name :: String.Chars.t, tags, field_value | fields) :: :ok
+
+  @doc """
+  Should be the same as `write(name, [], fields)`.
+  """
+  @callback write(name :: String.Chars.t, field_value | fields) :: :ok
+
+  @doc """
+  Should be the same as `measure(name, [], [], fun)`.
+  """
+  @callback measure(name :: String.Chars.t, fun :: (() -> result)) ::
+    result when result: any
+
+  @doc """
+  Should be the same as `measure(name, tags, [], fun)`.
+  """
+  @callback measure(name :: String.Chars.t, tags, fun :: (() -> result)) ::
+    result when result: any
+
+  @doc """
+  Measures the execution time of `fun` and writes it as a metric named `name`.
+
+  This function is just an utility function to measure the execution time of a
+  given function `fun`. The `name` and `tags` arguments work in the same way as
+  in `c:write/3`.
+
+  `fun`'s execution time is prepended as a field called `value` to the already
+  existing list of `fields`. This means that if there's already a field called
+  `value` in `fields`, it will be overridden by the measurement. This also means
+  that `fields` must be a list of key-value pairs (field name and value): simple
+  floats, integers, booleans, and binaries as values for `fields` are not
+  supported like they are in `c:write/3`.
+
+  This function returns whatever `fun` returns.
+
+  ## Examples
+
+  Assuming a `MyApp.Fluxter` Fluxter pool exists:
+
+      iex> MyApp.Fluxter.measure "task_exec_time", [host: "us-east"], fn ->
+      ...>   1 + 1
+      ...> end
+      2
+
+  """
+  @callback measure(name :: String.Chars.t, tags, fields, fun :: (() -> result)) ::
+    result when result: any
+
   @doc false
   defmacro __using__(_opts) do
     quote [unquote: false, location: :keep] do
+      @behaviour Fluxter
+
       @pool_size Application.get_env(__MODULE__, :pool_size, 5)
       @worker_names Enum.map(0..(@pool_size - 1), &:'#{__MODULE__}-#{&1}')
 
