4ab778830c2c2c0b1ac344be6d4e929909d0fbfa
Aleksei Magusev <lexmag@me.com>
2016-12-23 11:24:18 +0100

    Release v0.5.0

--- mix.exs
+++ mix.exs
@@ -4,7 +4,7 @@ defmodule Fluxter.Mixfile do
   def project() do
     [app: :fluxter,
      name: "Fluxter",
-     version: "0.4.0",
+     version: "0.5.0",
      elixir: "~> 1.2",
      package: package(),
      description: desc(),
