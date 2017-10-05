898e37e1cfe543d92037f85054b3d8e892f92e7c
Aleksei Magusev <lexmag@me.com>
2017-10-05 22:25:30 +0200

    Share EchoServer across all tests

--- test/fluxter_test.exs
+++ test/fluxter_test.exs
@@ -2,79 +2,87 @@ defmodule FluxterTest do
   use ExUnit.Case
 
   defmodule EchoServer do
-    def start(test, port) do
-      {:ok, sock} = :gen_udp.open(port, [:binary, active: false])
-      Task.start_link(fn ->
-        recv(test, sock)
-      end)
+    use GenServer
+
+    def start_link(port) do
+      GenServer.start_link(__MODULE__, port)
     end
 
-    defp recv(test, sock) do
-      send(test, {:echo, recv(sock)})
-      recv(test, sock)
+    def set_current_test(server, test) do
+      GenServer.call(server, {:set_current_test, test})
     end
 
-    defp recv(sock) do
-      case :gen_udp.recv(sock, 0) do
-        {:ok, {_, _, packet}} ->
-          packet
-        {:error, _} = error ->
-          error
+    def init(port) do
+      {:ok, socket} = :gen_udp.open(port, [:binary, active: true])
+      {:ok, %{socket: socket, test: nil}}
+    end
+
+    def handle_call({:set_current_test, current_test}, _from, %{test: test} = state) do
+      if is_nil(test) or is_nil(current_test) do
+        {:reply, :ok, %{state | test: current_test}}
+      else
+        {:reply, :error, state}
       end
+    end
+
+    def handle_info({:udp, socket, _, _, packet}, %{socket: socket, test: test} = state) do
+      send(test, {:echo, packet})
+      {:noreply, state}
     end
   end
 
-  defmodule Sample do
+  defmodule FluxterSample do
     use Fluxter
   end
 
   setup_all do
-    {:ok, _} = Sample.start_link
-    :ok
+    {:ok, server} = EchoServer.start_link(8092)
+    {:ok, _} = FluxterSample.start_link()
+    {:ok, %{server: server}}
   end
 
-  setup do
-    {:ok, _} = EchoServer.start(self(), 8092)
-    :ok
+  setup %{server: server} do
+    :ok = EchoServer.set_current_test(server, self())
+    on_exit(fn -> EchoServer.set_current_test(server, nil) end)
   end
 
   test "write/2,3" do
-    Sample.write("foo", bar: 11, baz: 0)
+    FluxterSample.write("foo", bar: 11, baz: 0)
     assert_receive {:echo, "foo bar=11i,baz=0i"}
 
-    Sample.write("foo bar", 11)
+    FluxterSample.write("foo bar", 11)
     assert_receive {:echo, "foo\\ bar value=11i"}
 
-    Sample.write("foo", 1.0)
+    FluxterSample.write("foo", 1.0)
     payload = "foo value=#{Float.to_string(1.0)}"
     assert_receive {:echo, ^payload}
 
-    Sample.write("foo", "data\"")
+    FluxterSample.write("foo", "data\"")
     assert_receive {:echo, "foo value=\"data\\\"\""}
 
-    Sample.write('foo', true)
+    FluxterSample.write('foo', true)
     assert_receive {:echo, "foo value=true"}
-    Sample.write("foo", false)
+    FluxterSample.write("foo", false)
     assert_receive {:echo, "foo value=false"}
 
-    Sample.write("foo", [bar: "baz qux"], 0)
+    FluxterSample.write("foo", [bar: "baz qux"], 0)
     assert_receive {:echo, "foo,bar=baz\\ qux value=0i"}
 
-    Sample.write("foo", [bar: "baz", qux: "baz"], 0)
+    FluxterSample.write("foo", [bar: "baz", qux: "baz"], 0)
     assert_receive {:echo, "foo,bar=baz,qux=baz value=0i"}
 
     refute_receive _any
   end
 
   test "measure/2,3,4" do
-    result = Sample.measure("foo", fn ->
+    result = FluxterSample.measure("foo", fn ->
       :timer.sleep(100)
       "OK"
     end)
     assert_receive {:echo, <<"foo value=10", _::4-bytes, "i">>}
     assert result == "OK"
 
-    result = Sample.measure("foo", [bar: "baz"], fn ->
+    result = FluxterSample.measure("foo", [bar: "baz"], fn ->
       :timer.sleep(100)
       "OK"
     end)
@@ -85,34 +93,34 @@ defmodule FluxterTest do
   end
 
   test "counter functionality" do
-    counter = Sample.start_counter("bar")
+    counter = FluxterSample.start_counter("bar")
 
     assert is_pid(counter)
 
-    assert Sample.flush_counter(counter) == :ok
+    assert FluxterSample.flush_counter(counter) == :ok
     refute_receive _any
     refute_alive counter
 
-    counter = Sample.start_counter("foo", [bar: "baz"])
+    counter = FluxterSample.start_counter("foo", [bar: "baz"])
 
-    assert Sample.increment_counter(counter, 2) == :ok
+    assert FluxterSample.increment_counter(counter, 2) == :ok
     refute_receive _any
 
-    assert Sample.increment_counter(counter, 1) == :ok
+    assert FluxterSample.increment_counter(counter, 1) == :ok
     refute_receive _any
 
-    assert Sample.flush_counter(counter) == :ok
+    assert FluxterSample.flush_counter(counter) == :ok
     assert_receive {:echo, "foo,bar=baz value=3i"}
 
     refute_receive _any
     refute_alive counter
 
-    counter = Sample.start_counter("qux", [], [bar: "baz"])
+    counter = FluxterSample.start_counter("qux", [], [bar: "baz"])
 
-    assert Sample.increment_counter(counter, 1.0) == :ok
+    assert FluxterSample.increment_counter(counter, 1.0) == :ok
     refute_receive _any
 
-    assert Sample.flush_counter(counter) == :ok
+    assert FluxterSample.flush_counter(counter) == :ok
     payload = "qux value=#{Float.to_string(1.0)},bar=\"baz\""
     assert_receive {:echo, ^payload}
 
@@ -121,7 +129,7 @@ defmodule FluxterTest do
 
     parent = self()
     spawn(fn ->
-      counter = Sample.start_counter("bar")
+      counter = FluxterSample.start_counter("bar")
       send(parent, {:counter, counter})
       exit(:timeout)
     end)
